0d4e8a4caaee84ed354fa6212ed7391d19adaee9
Abubakir1992 <79552141+Abubakir1992@users.noreply.github.com>
2023-05-31 16:55:33 -0400

    test next loaded video workpalse

--- src/videolist.js
+++ src/videolist.js
@@ -1,33 +1,45 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import VideoPlayer from './youtube.js';
-import SearchBar from './Search.js';
+import React, { useState } from "react";
+import axios from "axios";
+import VideoPlayer from "./youtube.js";
+import SearchBar from "./Search.js";
 
-const API_KEY = 'AIzaSyA0NxPeGiiw3nYZFYT9U_jv2wcQqgjyeQ0';
+const API_KEY = "AIzaSyA0NxPeGiiw3nYZFYT9U_jv2wcQqgjyeQ0";
 
 const VideoList = () => {
   const [videos, setVideos] = useState([]);
-  const [selectedVideoId, setSelectedVideoId] = useState('');
-  const [currentVideoIndex, setCurrentVideoIndex] = useState(0)
+  const [selectedVideoId, setSelectedVideoId] = useState("");
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
 
   const loadNextVideo = () => {
     const nextVideoId = videos[currentVideoIndex + 1].id.videoId; // % videos[currentVideoIndex].id.videoId.length;
-    setCurrentVideoIndex(currentVideoIndex + 1)
-    setSelectedVideoId(nextVideoId)
-    console.log(videos)
-  }
+    setCurrentVideoIndex(currentVideoIndex + 1);
+    setSelectedVideoId(nextVideoId);
+    console.log(videos);
+
+  };
+
+  const findIndex = (tag) => {
+    const videoList = Array.from(videos);
+    console.log(videoList, tag)
+    const indexValue = videoList.findIndex(item => item.etag === tag);
+    console.log(indexValue)
+    setCurrentVideoIndex(indexValue)
+  };
+    
 
   const handleSearch = async (search) => {
     try {
-      const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
-        params: {
-          part: 'snippet',
-          maxResults: 50,
-          key: API_KEY
-          ,
-          q: search,
-        },
-      });
+      const response = await axios.get(
+        "https://www.googleapis.com/youtube/v3/search",
+        {
+          params: {
+            part: "snippet",
+            maxResults: 50,
+            key: API_KEY,
+            q: search,
+          },
+        }
+      );
 
       setVideos((prevVideos) => [...prevVideos, ...response.data.items]);
       setVideos(response.data.items);
@@ -35,12 +47,13 @@ const VideoList = () => {
         setSelectedVideoId(response.data.items[0].id.videoId);
       }
     } catch (error) {
-      console.error('Error fetching videos:', error);
+      console.error("Error fetching videos:", error);
     }
   };
 
-  const handleVideoSelect = (videoId) => {
+  const handleVideoSelect = (videoId, etag) => {
     setSelectedVideoId(videoId);
+    findIndex(etag)
   };
 
   return (
@@ -49,12 +62,23 @@ const VideoList = () => {
       <SearchBar handleSearch={handleSearch} />
       <div>
         {videos.map((video) => (
-          <div className={`box box-${video.id.videoId}`} key={video.id.videoId} onClick={() => handleVideoSelect(video.id.videoId)}>
-            <h2>{video.snippet.title}</h2>
-            <img src={video.snippet.thumbnails.default.url} alt={video.snippet.title} />
+          <div
+            className={`box box-${video.id.videoId}`}
+            key={video.id.videoId}
+            onClick={() => handleVideoSelect(video.id.videoId, video.etag)}
+          >
+            <form>
+              <h2>{video.snippet.title}</h2>
+            </form>
+            <img
+              src={video.snippet.thumbnails.default.url}
+              alt={video.snippet.title}
+            />
           </div>
         ))}
-</div>
-</div>)}
+      </div>
+    </div>
+  );
+};
 
-export default VideoList
+export default VideoList;
